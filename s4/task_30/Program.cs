8507fadd0465eeabd02797edf2c6bd83897d018f
NikitaGolaev <golaevnikita@gmail.com>
2022-02-21 19:59:41 +0300

    Edited task_30

--- s4/task_30/Program.cs
+++ s4/task_30/Program.cs
@@ -5,15 +5,4 @@ Console.Clear();
 System.Console.Write("Введите положительное число: ");
 int num = int.Parse(Console.ReadLine());
 
-int count = 1;
-
-while (count <= num)
-{
-    if (count * count * count % 2 == 0)
-    {
-        int i = count * count * count;
-        System.Console.WriteLine($"Куб числа {count} из диапазона от 1 до {num} равен {i}, что является его чётным кубом");
-        count++;
-    }
-    else count++;
-}
+for (int i = 2; i <= num; i += 2) System.Console.WriteLine(i * i * i);

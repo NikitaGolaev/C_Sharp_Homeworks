fb37cf878026d64dc5b2711aed7f721182de3647
NikitaGolaev <golaevnikita@gmail.com>
2022-02-23 23:51:29 +0300

    Solved task_42

---
+++ s6/task_42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+Console.Clear();
+
+System.Console.Write("Введите положительное число: ");
+string numInString = Console.ReadLine();
+int length = numInString.Length;
+
+int num = int.Parse(numInString);
+// System.Console.WriteLine($"Число {num1}: {num1.GetType()}");
+
+if (num < 0) num = -(num);
+
+int count = 0;
+
+for (int i = 0; i <= length; i++) 
+{
+    if (num % 10 > 0) count++;
+
+    num = num / 10;
+}
+
+System.Console.WriteLine($"Цифр больше 0: {count}");
---
+++ s6/task_44/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, если b1, k1, b2 и k2 заданы.
+
+Console.Clear();
+
+// int k1 = 2, b1 = 3, k2 = 4, b2 = 5;
+int k1 = 2, b1 = -1, k2 = -3, b2 = 1;
+
+// y = k1 * x + b1;
+// y = k2 * x + b2;
+
+// System.Console.WriteLine(y);
+
+int n = k1 * x + b1 - k2 * x - b2;
+
+// System.Console.WriteLine(x);
+// System.Console.WriteLine(y);
+System.Console.WriteLine(n);
+

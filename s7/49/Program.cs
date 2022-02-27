00e2ad58746c9b7a03e787afe2abc543b855079c
NikitaGolaev <golaevnikita@gmail.com>
2022-02-27 23:04:37 +0300

    Added task_49

---
+++ s7/49/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+// Вопрос в 22 строке.
+
+Console.Clear();
+
+System.Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+
+System.Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] twoArray = new double[m, n];
+
+void FillingMethod(double[,] argument)
+{
+    for (int i = 0; i < argument.GetLength(0); i++)
+    {
+        for (int j = 0; j < argument.GetLength(1); j++)
+        {
+            // argument[i, j] = new Random().Next(1, 10);
+            argument[i, j] = new Random().NextDouble(); // Получаются слишком сложные числа. Что можно с этим сделать?
+        }
+    }
+}
+
+void PrintMethod(double[,] argument)
+{
+    for (int i = 0; i < argument.GetLength(0); i++)
+    {
+        for (int j = 0; j < argument.GetLength(1); j++)
+        {
+            System.Console.Write($"{argument[i, j]} ");
+        }
+
+        System.Console.WriteLine();
+    }
+}
+
+FillingMethod(twoArray);
+PrintMethod(twoArray);

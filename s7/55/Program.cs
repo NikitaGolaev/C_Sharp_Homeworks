ead3c5794a8f4dd9fb723cb700fddb0263b4bdce
NikitaGolaev <golaevnikita@gmail.com>
2022-03-03 00:28:26 +0300

    Added task_55

--- s7/55/Program.cs
+++ s7/55/Program.cs
@@ -2,4 +2,44 @@
 
 Console.Clear();
 
+System.Console.Write("Количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+System.Console.Write("Количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
 
+int[,] arr = new int[m, n];
+
+void FillingMethod(int[,] argument)
+{
+    for (int i = 0; i < argument.GetLength(0); i++)
+    {
+        for (int j = 0; j < argument.GetLength(1); j++)
+        {
+            argument[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+FillingMethod(arr);
+
+void PrintMethod(int[,] argument)
+{
+    for (int i = 0; i < argument.GetLength(0); i++)
+    {
+        for (int j = 0; j < argument.GetLength(1); j++)
+        {
+            System.Console.Write($"{argument[i, j]} ");
+        }
+
+        System.Console.WriteLine();
+    }
+}
+
+PrintMethod(arr);
+System.Console.WriteLine();
+
+System.Console.WriteLine("Среднее арифметическое столбцов массива: ");
+for (int j = 0; j < arr.GetLength(1); j++)
+{    
+    System.Console.WriteLine((j + 1) + " - " + ((double) (arr[0, j] + arr[1, j] + arr[2, j]) / m)); // 'm' - количество строк массива.
+}

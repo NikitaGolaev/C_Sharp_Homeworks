c5e9b669f1e507cae2861bd3ae65e9e6b6ef2095
NikitaGolaev <golaevnikita@gmail.com>
2022-03-03 22:03:12 +0300

    Added task_51

--- s7/51/Program.cs
+++ s7/51/Program.cs
@@ -2,4 +2,14 @@
 
 Console.Clear();
 
+int[,] arr = new int[3, 3];
 
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = i + j;
+        System.Console.Write($"{arr[i, j]} ");
+    }
+    System.Console.WriteLine();
+}

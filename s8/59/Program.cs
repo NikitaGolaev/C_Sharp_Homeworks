bde231090edeabc99dc9df7254c39b237e53d5ec
NikitaGolaev <golaevnikita@gmail.com>
2022-03-03 22:57:26 +0300

    Added task_59

--- s8/59/Program.cs
+++ s8/59/Program.cs
@@ -1,3 +1,37 @@
 ﻿// Задача 59: В прямоугольной матрице найти строку с наименьшей суммой элементов.
 
+Console.Clear();
 
+int[,] arr = new int[3, 3];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(1, 10);
+        System.Console.Write($"{arr[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+
+int sum = 0;
+int max = 0;
+int str = 0; // Индекс строки
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    sum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        sum = arr[i, j] + sum;        
+    }
+    if (sum > max)
+    {
+        max = sum;
+        str = i;
+    }    
+}
+
+System.Console.WriteLine($"Индекс строки: {str}");
+System.Console.WriteLine($"Сумма чисел строки: {max}");
